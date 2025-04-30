fdbaaaa8a0c3c82d66b6db10e03d95ef37cd2863
bahlool1401 <bahlool.ramesh@gmail.com>
2025-04-30 23:57:21 +0330

    edit in schema

--- schemas/signup.schema.ts
+++ schemas/signup.schema.ts
@@ -1,7 +1,11 @@
 import {z} from "zod"
 
 export default z.object({
-    
+    name : z.string().min(3).max(50),
     email:z.string().email(),
-    password :z.string().min(8)
+    password :z.string().min(8),
+    passwordConfirn:z.string().min(8)
+}).refine(data=>data.password===data.passwordConfirn,{
+    message : 'Password do not mach',
+    path:['passwordConfirn']
 })
--- server/api/auth/signup.post.ts
+++ server/api/auth/signup.post.ts
@@ -1,6 +1,8 @@
-
+import {validator} from 'nuxt-server-utils'
 
 export default defineEventHandler(async (event)=>{
     const body = await readBody(event)
+    validator.validateSchema(signupSchema,body)
+
     return "ok"
 })
