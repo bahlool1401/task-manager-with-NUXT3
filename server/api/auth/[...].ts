cc466543d271e1fd191e73754e77a4845556ce25
bahlool1401 <bahlool.ramesh@gmail.com>
2025-05-25 00:24:00 +0330

    edit in nuxtauthhandler

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,4 +1,14 @@
+import { NuxtAuthHandler } from "#auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { User } from "~/server/models/User";
+
 
 export default nuxtAuthHandler({
-    secret : useRuntimeConfig().auth.secret
+    secret : useRuntimeConfig().auth.secret,
+    pages: {
+        signIn : "/auth/signin"
+    },
+    providers:[
+        
+    ]
 })
